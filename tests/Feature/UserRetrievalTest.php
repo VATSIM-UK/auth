62e8e8fcdf9954d59dc25f6d294834370a301ff0
Alex Toff <me@alextoff.uk>
2019-08-19 19:48:53 +0100

    Add tests for API route

---
+++ tests/Feature/UserRetrievalTest.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Tests\Feature;
+
+
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+
+class UserRetrievalTest extends TestCase
+{
+    /* @test */
+    public function testUnauthenticatedUserCannotAccessUserInformation()
+    {
+        $this->json('GET', route('api.user'))
+            ->assertUnauthorized();
+    }
+
+    /* @test */
+    public function testAuthenticatedUserCanAccessTheirInformation()
+    {
+        Passport::actingAs($this->user);
+
+        $this->json('GET', route('api.user'))
+            ->assertSuccessful()
+            ->assertJsonFragment([
+                'id' => $this->user->id,
+                'email' => $this->user->email,
+            ]);
+    }
+}
