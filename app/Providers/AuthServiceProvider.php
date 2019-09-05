4f8b99837842f2856c6a534c3fa39b685cb38cf3
Alex Toff <me@alextoff.uk>
2019-09-05 14:33:09 +0100

    Add check for table

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Passport\Client;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Schema;
 use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
@@ -20,7 +21,7 @@ class AuthServiceProvider extends ServiceProvider
         Passport::useClientModel(Client::class);
 
         // Enable personal access client on development environments
-        if(App::environment('local') && $client = Client::where('personal_access_client', true)->first(['id'])){
+        if(App::environment('local') && Schema::hasTable('oauth_clients') && $client = Client::where('personal_access_client', true)->first(['id'])){
             Passport::personalAccessClientId($client->id);
         }
     }
