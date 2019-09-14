78378fd8ab2bacb65825e6bee726a48668a1e854
Alex Toff <atoff@users.noreply.github.com>
2019-09-14 10:23:09 +0100

    Change in API route authentication (#13)
    * Implement Machine-Machine authentication
    
    * Fix travis deployment
    
    * Fix tests
    
    * Merge Auth and CheckClient middlewares into one
    
    * Remove unused setup

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,9 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\ApiClientOrUserAuthentication;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Laravel\Passport\Http\Middleware\CheckClientCredentials;
 
 class Kernel extends HttpKernel
 {
@@ -59,6 +61,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'auth.api.multi' => ApiClientOrUserAuthentication::class
     ];
 
     /**
