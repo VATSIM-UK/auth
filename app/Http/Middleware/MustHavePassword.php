be9e1e01d79c7c47c26f65fad7d2adb05d04a482
Alex Toff <me@alextoff.uk>
2019-12-20 19:20:11 +0000

    Unit test new middleware

--- app/Http/Middleware/MustHavePassword.php
+++ app/Http/Middleware/MustHavePassword.php
@@ -4,20 +4,23 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\UnauthorizedException;
 
 class MustHavePassword
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        $user = Auth::user() ? Auth::user() : Auth::guard('partial_web')->user();
-        if (! $user->hasPassword()) {
+        $user = Auth::user() ?? Auth::guard('partial_web')->user();
+        throw_if(!$user, new UnauthorizedException());
+
+        if (!$user->hasPassword()) {
             return redirect('/');
         }
 
---
+++ tests/Unit/Middleware/MustHavePasswordMiddlewareTest.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace Tests\Unit\Middleware;
+
+
+use App\Http\Middleware\MustHavePassword;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\UnauthorizedException;
+use Tests\TestCase;
+
+class MustHavePasswordMiddlewareTest extends TestCase
+{
+    /** @test */
+    public function itPassesIfTheyHavePassword()
+    {
+        $this->user->setPassword('Testing1234');
+        $this->assertTrue($this->user->hasPassword());
+
+        $this->actingAs($this->user);
+        $request = new Request();
+        $middleware = resolve(MustHavePassword::class);
+        $response = $middleware->handle($request, function () {
+        });
+
+        $this->assertNull($response);
+
+        $this->actingAs($this->user, 'partial_web');
+        $request = new Request();
+        $middleware = resolve(MustHavePassword::class);
+        $response = $middleware->handle($request, function () {
+        });
+
+        $this->assertNull($response);
+    }
+
+    /** @test */
+    public function itRedirectsIfTheyDontHavePassword()
+    {
+        $this->assertFalse($this->user->hasPassword());
+
+        $this->actingAs($this->user);
+        $request = new Request();
+        $middleware = resolve(MustHavePassword::class);
+        $response = $middleware->handle($request, function () {
+        });
+
+        $this->assertEquals(get_class($response), RedirectResponse::class);
+    }
+
+    /** @test */
+    public function itThrowsExceptionIfNotAuthenticated()
+    {
+        $this->expectException(UnauthorizedException::class);
+
+        $request = new Request();
+        $middleware = resolve(MustHavePassword::class);
+        $middleware->handle($request, function () {
+        });
+    }
+}
