ed62da2349b9a1b25a29d17c8fba5f116382fa78
Alex Toff <me@alextoff.uk>
2020-02-06 20:00:03 +0000

    Fix bug

--- app/GraphQL/Queries/UserPermissions.php
+++ app/GraphQL/Queries/UserPermissions.php
@@ -8,11 +8,11 @@ class UserPermissions
 {
     public function directPermissions($rootValue, array $args)
     {
-        return User::with(['permissions'])->findOrFail($args['user_id'])->pluck('permission');
+        return User::with('permissions')->findOrFail($args['user_id'])->permissions()->pluck('permission');
     }
 
     public function rolePermissions($rootValue, array $args)
     {
-        return User::with(['roles'])->findOrFail($args['user_id'])->getPermissionsViaRoles();
+        return User::with('roles')->findOrFail($args['user_id'])->getPermissionsViaRoles();
     }
 }
